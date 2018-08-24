76e36cb98b13e8b6d1a015bc6df46aaad526cad5
kuldeep349 <349kuldeep@gmail.com>
2018-08-24 08:09:15 +0530

    radius filter

--- myfirst.js
+++ myfirst.js
@@ -10,9 +10,8 @@ app.use(cors())
 
 app.get('/', function (req, res, next) {
     var q = url.parse(req.url, true).query;
-    //?sort=monthly-price-asc&radius=1500&postcode=cv12ue&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New&make=HONDA&model=CIVIC&aggregatedTrim=SR&min-monthly-price=300&max-monthly-price=1000&deposit=1000&term=36&yearly-mileage=10000
-//    axios.get('https://www.autotrader.co.uk/car-search?postcode=cv12ue&radius=10&make=' + q.make + '&page=' + q.page + '&sort=default&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New&search-target=usedcars')
-    var path = 'https://www.autotrader.co.uk/car-search?sort=monthly-price-asc&radius=' + q.radius + '&postcode=' + q.postcode + '&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New&make=' + q.make + '&model=' + q.model + '&aggregatedTrim=SR&min-monthly-price=' + q.min_monthly_price + '&max-monthly-price=' + q.max_monthly_price + '&deposit=' + q.deposit + '&term=36&yearly-mileage=' + q.yearly_mileage;
+    //  var path = 'https://www.autotrader.co.uk/car-search?sort=monthly-price-asc&radius=' + q.radius + '&postcode=' + q.postcode + '&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New&make=' + q.make + '&model=' + q.model + '&aggregatedTrim=SR&min-monthly-price=' + q.min_monthly_price + '&max-monthly-price=' + q.max_monthly_price + '&deposit=' + q.deposit + '&term=36&yearly-mileage=' + q.yearly_mileage;
+    var path = 'https://www.autotrader.co.uk/car-search?postcode=cv12ue&radius=' + q.radius + '&make=' + q.make + '&page=' + q.page + '&sort=default&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New&search-target=usedcars&colour=' + q.colour + '&fuel-type=' + q.fuel_type;
     axios.get(path)
             .then((response) => {
                 if (response.status === 200) {
